0c0a79f4b7cea6123d89d0c3df8370e8a3046014
n7tms@yahoo.com <n7tms@yahoo.com>
2024-11-19 16:50:03 -0700

    05 in progress

--- 2024/05.py
+++ 2024/05.py
@@ -10,7 +10,7 @@ IN_FILE1 = os.path.join("2024","inputs","2024-05-1.txt")
 # IN_FILE2 = os.path.join("2024","inputs","2024-05-2.sample.txt")
 IN_FILE2 = os.path.join("2024","inputs","2024-05-2.txt")
 # IN_FILE3 = os.path.join("2024","inputs","2024-05-3.sample.txt")
-# IN_FILE3 = os.path.join("2024","inputs","2024-05-3.txt")
+IN_FILE3 = os.path.join("2024","inputs","2024-05-3.txt")
 
 
 
@@ -66,7 +66,7 @@ def part1(clappers, rounds):           # => 4223
     return firsts[::-1]
 
 
-def part2(clappers, rounds):            # => 20413535199405
+def part2(clappers, rounds):            # => 20580403334550
     shouts = {}
     clapper_size = len(clappers)
     rnd = 0
@@ -89,9 +89,11 @@ def part2(clappers, rounds):            # => 20413535199405
         
         # Add these first to dictionary
         firsts = ""
-        for rw in clappers:
-            firsts = firsts + str(rw[0])
-        shout = firsts[::-1]
+        for rw in range(len(clappers)-1,-1,-1):
+            firsts = firsts + str(clappers[rw][0])
+        # shout = firsts[::-1]
+        shout = firsts
+
         if shout in shouts:
             shouts[shout] += 1
         else: 
@@ -103,29 +105,64 @@ def part2(clappers, rounds):            # => 20413535199405
 
 
 
-def part3():       # => 
-    pass
+def part3(clappers, rounds):       # => 
+    shouts = {}
+    clapper_size = len(clappers)
+    rnd = 0
+    while True:
+        rnd += 1
+        # Determine head row
+        r1 = rnd % clapper_size
+        if r1 == 0:
+            head_row = 0
+            clap_row = clapper_size - 1
+        else:
+            head_row = clapper_size - r1
+            clap_row = head_row - 1
+
+        clapper = clappers[head_row].pop(0)
+        if clapper > len(clappers[clap_row]):
+            clappers[clap_row].insert(len(clappers[clap_row]) - (clapper - len(clappers[clap_row]))+1, clapper)
+        else:
+            clappers[clap_row].insert(clapper-1, clapper)
+        
+        # Add these first to dictionary
+        firsts = ""
+        for rw in range(len(clappers)-1,-1,-1):
+            firsts = firsts + str(clappers[rw][0])
+        # shout = firsts[::-1]
+        shout = int(firsts)
+
+        if shout in shouts:
+            shouts[shout] += 1
+        else: 
+            shouts[shout] = 1
+        
+        if shouts[shout] == rounds:
+            return max(shouts)
+           
+
 
 
 def solve():
     """Solve the puzzle for the given input."""
-    data = parse(IN_FILE1)
-    start_time = time.time()
-    p1 = str(part1(data, 10))
-    exec_time = time.time() - start_time
-    print(f"part 1: {p1} ({exec_time:.4f} sec)")
-
-    data = parse(IN_FILE2)
-    start_time = time.time()
-    p2 = str(part2(data, 2024))
-    exec_time = time.time() - start_time
-    print(f"part 2: {p2} ({exec_time:.4f} sec)")
-
-    # data = parse(IN_FILE3)
+    # data = parse(IN_FILE1)
     # start_time = time.time()
-    # p3 = str(part3(data))
+    # p1 = str(part1(data, 10))
     # exec_time = time.time() - start_time
-    # print(f"part 3: {p3} ({exec_time:.4f} sec)")
+    # print(f"part 1: {p1} ({exec_time:.4f} sec)")
+
+    # data = parse(IN_FILE2)
+    # start_time = time.time()
+    # p2 = str(part2(data, 2024))
+    # exec_time = time.time() - start_time
+    # print(f"part 2: {p2} ({exec_time:.4f} sec)")
+
+    data = parse(IN_FILE3)
+    start_time = time.time()
+    p3 = str(part3(data, 10000000))
+    exec_time = time.time() - start_time
+    print(f"part 3: {p3} ({exec_time:.4f} sec)")
 
 
 if __name__ == "__main__":
