351d470515b22a49d1d77eaf067e24449c4d3629
n7tms@yahoo.com <n7tms@yahoo.com>
2024-11-21 13:42:24 -0700

    07 in progress

--- 2024/07.py
+++ 2024/07.py
@@ -5,9 +5,9 @@ import os
 import time
 
 
-IN_FILE1 = os.path.join("2024","inputs","2024-07-1.sample.txt")
-# IN_FILE1 = os.path.join("2024","inputs","2024-07-1.txt")
-# IN_FILE2 = os.path.join("2024","inputs","2024-07-2.sample.txt")
+# IN_FILE1 = os.path.join("2024","inputs","2024-07-1.sample.txt")
+IN_FILE1 = os.path.join("2024","inputs","2024-07-1.txt")
+IN_FILE2 = os.path.join("2024","inputs","2024-07-2.sample.txt")
 # IN_FILE2 = os.path.join("2024","inputs","2024-07-2.txt")
 # IN_FILE3 = os.path.join("2024","inputs","2024-07-3.sample.txt")
 # IN_FILE3 = os.path.join("2024","inputs","2024-07-3.txt")
@@ -26,26 +26,57 @@ def parse(IN_FILE):
     for line in data:
         c, actions = line.split(":")
         if c not in chariots:
-            chariots[c] = [actions.split(","), []]
+            chariots[c] = {"Actions":actions.split(","), "Powers":[], "Total":0}
     return chariots
     
 
-def part1(chariots):           # => 
+def part1(chariots):           # => GFDKAJIHB
     for chariot, data in chariots.items():
         pwr = 10
-        actions = data[0]
+        actions = chariots[chariot]["Actions"]
+        pwrs = []
         for x in range(10):
             action = actions[x % len(actions)]
             if action == "+":
                 pwr += 1
             elif action == "-":
                 pwr -= 1
-            
+            chariots[chariot]["Powers"].append(pwr)
+        chariots[chariot]["Total"] = sum(chariots[chariot]["Powers"])
+        
+    # probably not the most efficient, but create a dictionary of just the chariots and total powers...
+    totals = {}
+    for c,d in chariots.items():
+        totals[c] = d["Total"]
+    
+    # ... sort them, reverse them, and return them
+    out = sorted(totals.items(), key=lambda item: item[1])
+    out2 = [t[0] for t in out]
+    out2.reverse()
+    return ''.join(out2)
+
 
     
 
-def part2(nails):            # => 
-    pass
+def part2(chariots):            # => 
+    # track = "S-=++=-==++=++=-=+=-=+=+=--=-=++=-==++=-+=-=+=-=+=+=++=-+==++=++=-=-=---=++==--+++==++=+=--==++==+++=++=+++=--=+=-=+=-+=-+=-+-=+=-=+=-+++=+==++++==---=+=+=-S"
+    track = "+===++-=+=-S"
+
+    loops = 10
+    for loop in range(loops):
+        for s, segment in enumerate(track):
+            for chariot, data in chariots.items():
+                pwr = 10
+                actions = chariots[chariot]["Actions"]
+                pwrs = []
+                for x in range(10):
+                    action = actions[x % len(actions)]
+                    if action == "+":
+                        pwr += 1
+                    elif action == "-":
+                        pwr -= 1
+                    chariots[chariot]["Powers"].append(pwr)
+                chariots[chariot]["Total"] = sum(chariots[chariot]["Powers"])
 
 
 def part3(nails):       # => 
@@ -60,11 +91,11 @@ def solve():
     exec_time = time.time() - start_time
     print(f"part 1: {p1} ({exec_time:.4f} sec)")
 
-    # data = parse(IN_FILE2)
-    # start_time = time.time()
-    # p2 = str(part2(data))
-    # exec_time = time.time() - start_time
-    # print(f"part 2: {p2} ({exec_time:.4f} sec)")
+    data = parse(IN_FILE2)
+    start_time = time.time()
+    p2 = str(part2(data))
+    exec_time = time.time() - start_time
+    print(f"part 2: {p2} ({exec_time:.4f} sec)")
 
     # data = parse(IN_FILE3)
     # start_time = time.time()
