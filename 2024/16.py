4e53dd2c96bfcefa123c56619a6a4a1bd4d9fc95
n7tms@yahoo.com <n7tms@yahoo.com>
2024-11-27 15:06:21 -0700

    16 p2 in progress

--- 2024/16.py
+++ 2024/16.py
@@ -5,9 +5,9 @@ import os
 import time
 
 
-IN_FILE1 = os.path.join("2024","inputs","2024-16-1.sample.txt")
-# IN_FILE1 = os.path.join("2024","inputs","2024-16-1.txt")
-# IN_FILE2 = os.path.join("2024","inputs","2024-16-2.sample.txt")
+# IN_FILE1 = os.path.join("2024","inputs","2024-16-1.sample.txt")
+IN_FILE1 = os.path.join("2024","inputs","2024-16-1.txt")
+IN_FILE2 = os.path.join("2024","inputs","2024-16-2.sample.txt")
 # IN_FILE2 = os.path.join("2024","inputs","2024-16-2.txt")
 # IN_FILE3 = os.path.join("2024","inputs","2024-16-3.sample.txt")
 # IN_FILE3 = os.path.join("2024","inputs","2024-16-3.txt")
@@ -21,15 +21,76 @@ def parse(IN_FILE):
 
     with open(IN_FILE) as fp:
         data = fp.read().splitlines()
+    
+    positions, cat_faces = data[0], data[2:]
+    pulls = [int(x) for x in positions.split(',')]
 
-    return data
+    wheels = {}
+    for x in range(len(pulls)):
+        wheels[x] = {'faces':[], 'spins':pulls[x]}
+    
+    for line in cat_faces:
+        index = 0
+        wheel = 0
+        while index < len(line):
+            if line[index] != ' ':
+                face = line[index:index+3]
+                wheels[wheel]['faces'].append(face)
+            index += 4
+            wheel += 1
+
+    return wheels
 
 
-def part1(data):           # => 
-    pass
+def part1(wheels):           # => <:- ^_^ <:< <.> 
 
-def part2(data):            # => 
-    pass
+    wheel_index = [0]*len(wheels)
+
+    for cnt in range(100):
+        for wheel, data in wheels.items():
+            faces, spins = data['faces'], data['spins']
+            wheel_index[wheel] += spins
+    
+    resulting_face = ''
+    for w, d in wheels.items():
+        faces = wheels[w]['faces']
+        resulting_face = resulting_face + faces[wheel_index[w] % len(faces)] + ' '
+    return resulting_face
+
+
+
+def part2(wheels):            # => 
+    wheel_index = [0]*len(wheels)
+    total_byte_coin = 0
+
+    for cnt in range(202420242024):
+        for wheel, data in wheels.items():
+            faces, spins = data['faces'], data['spins']
+            wheel_index[wheel] += spins
+    
+        resulting_face = ''
+        for w, d in wheels.items():
+            faces = wheels[w]['faces']
+            resulting_face = resulting_face + faces[wheel_index[w] % len(faces)]
+        # print(resulting_face)
+
+        # Extracting just the eyes
+        eye_pos = [0, 2, 3, 5, 6, 8, 9, 11]
+        extracted_characters = [resulting_face[i] for i in eye_pos if i < len(resulting_face)]
+
+        # Counting each character
+        character_counts = {}
+        for char in extracted_characters:
+            if char in character_counts:
+                character_counts[char] += 1
+            else:
+                character_counts[char] = 1
+
+        for _, v in character_counts.items():
+            if v > 2:
+                total_byte_coin += 1 + (v-3)
+
+    return total_byte_coin
 
 
 def part3(data):       # => 
@@ -50,11 +111,11 @@ def solve():
     exec_time = time.time() - start_time
     print(f"part 2: {p2} ({exec_time:.4f} sec)")
 
-    data = parse(IN_FILE3)
-    start_time = time.time()
-    p3 = str(part3(data))
-    exec_time = time.time() - start_time
-    print(f"part 3: {p3} ({exec_time:.4f} sec)")
+    # data = parse(IN_FILE3)
+    # start_time = time.time()
+    # p3 = str(part3(data))
+    # exec_time = time.time() - start_time
+    # print(f"part 3: {p3} ({exec_time:.4f} sec)")
 
 
 if __name__ == "__main__":
