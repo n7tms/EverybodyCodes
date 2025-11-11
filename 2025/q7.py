b01cb26409ef3420861870d8c2eebe9fd438699a
Todd Smith <n7tms@yahoo.com>
2025-11-10 17:01:24 -0700

    q7 prepped

---
+++ 2025/q7.py
@@ -0,0 +1,67 @@
+# EverybodyCodes - Quest 7
+
+import os
+import time
+
+
+IN_FILE1 = os.path.join("2025","inputs","2025-07.sample.txt")
+# IN_FILE1 = os.path.join("2025","inputs","2025-07-1.txt")
+# IN_FILE2 = os.path.join("2025","inputs","2025-07-2.txt")
+# IN_FILE3 = os.path.join("2025","inputs","2025-07-3.txt")
+
+
+
+def parse(IN_FILE):
+    """
+    Parse
+    """
+
+    with open(IN_FILE) as fp:
+        data = fp.read().splitlines()
+
+    i_data = [d for d in data[0]]
+
+    return i_data
+
+
+
+def part1(x):           # => 
+
+    return 0
+
+
+def part2(x):     # => 
+
+    return 0
+    
+
+def part3(x):           # => 
+
+    return 0
+
+
+def solve():
+    """Solve the puzzle for the given input."""
+    data = parse(IN_FILE1)
+    start_time = time.time()
+    p1 = str(part1(data))
+    exec_time = time.time() - start_time
+    print(f"part 1: {p1} ({exec_time:.4f} sec)")
+
+    # data = parse(IN_FILE2)
+    # start_time = time.time()
+    # p2 = str(part2(data))
+    # exec_time = time.time() - start_time
+    # print(f"part 2: {p2} ({exec_time:.4f} sec)")
+
+    # data = parse(IN_FILE3)
+    # start_time = time.time()
+    # p3 = str(part3(data))
+    # exec_time = time.time() - start_time
+    # print(f"part 3: {p3} ({exec_time:.4f} sec)")
+
+
+if __name__ == "__main__":
+    solve()
+
+
