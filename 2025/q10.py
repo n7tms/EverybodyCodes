bfe3044ca13e0d142fc18dd2905af74e03376cce
Todd Smith <n7tms@yahoo.com>
2025-11-15 12:23:11 -0700

    q10 in progress

--- 2025/q10.py
+++ 2025/q10.py
@@ -5,27 +5,81 @@ import time
 from itertools import combinations
 
 
-IN_FILE3 = os.path.join("2025","inputs","2025-10.sample.txt")
+IN_FILE1 = os.path.join("2025","inputs","2025-10.sample.txt")
 # IN_FILE1 = os.path.join("2025","inputs","2025-10-1.txt")
 # IN_FILE2 = os.path.join("2025","inputs","2025-10-2.txt")
 # IN_FILE3 = os.path.join("2025","inputs","2025-10-3.txt")
 
-
+dragon_moves = list()
+r_max = 0
+c_max = 0
+r_ctr, c_ctr = 0,0
 
 def parse(IN_FILE):
     """
     Parse
     """
+    global r_max, c_max, r_ctr, c_ctr
     with open(IN_FILE) as fp:
         data = fp.read().splitlines()
 
+    sheep = list()
+    for r, row in enumerate(data):
+        for c, col in enumerate(row):
+            if col == "S":
+                sheep.append([r,c])
+    
+    r_max = len(data)
+    c_max = len(data[0])
+    # find center of board
+    r_ctr = (len(data) // 2) + 1
+    c_ctr = (len(data[0]) // 2) + 1
+
+    return sheep
 
 
-    return data
+def get_dragon_moves(to_check: list, depth:int, target_depth: int):
+    global dragon_moves
+
+    loc = to_check.pop()
+    dragon_moves.append(loc)
+    if depth >= target_depth: return
+
+    r,c = loc
+    if r-2 >= 0 and c-1 >= 0:
+        to_check.append([r-2, c-1])
+    if r-2 >= 0 and c+1 < c_max:
+        to_check.append([r-2, c+1])
+    if r-1 >= 0 and c+2 < c_max:
+        to_check.append([r-1, c+2])
+    if r+1 < r_max and c+2 < c_max:
+        to_check.append([r+1, c+2])
+    if r+2 < r_max and c+1 < c_max:
+        to_check.append([r+2, c+1])
+    if r+2 < r_max and c-1 >= 0:
+        to_check.append([r+1, c-1])
+    if r+1 < r_max and c-2 >= 0:
+        to_check.append([r+1, c-2])
+    if r-1 >= 0 and c-2 >=0:
+        to_check.append([r-1, c-2])
+
+    if to_check:
+        get_dragon_moves(to_check, depth+1, target_depth)
+    else: 
+        return
+    
 
 
 
 def part1(data):     # => 
+
+
+    # find all possible dragon moves
+    get_dragon_moves([[r_ctr, c_ctr]], 0, 1)
+
+    # compare dragon moves with sheep locations
+
+    # return number of "intersections"
 
     return 0
 
