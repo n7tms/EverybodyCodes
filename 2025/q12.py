bc2007ce4a74c5ca22b343a01af6ada8fa2c52d3
Todd Smith <n7tms@yahoo.com>
2025-11-19 11:28:38 -0700

    q12 in progress

--- 2025/q12.py
+++ 2025/q12.py
@@ -4,9 +4,9 @@ import os
 import time
 
 
-IN_FILE1 = os.path.join("2025","inputs","2025-12.sample.txt")
-# IN_FILE1 = os.path.join("2025","inputs","2025-12-1.txt")
-# IN_FILE2 = os.path.join("2025","inputs","2025-12-2.txt")
+# IN_FILE2 = os.path.join("2025","inputs","2025-12.sample.txt")
+IN_FILE1 = os.path.join("2025","inputs","2025-12-1.txt")
+IN_FILE2 = os.path.join("2025","inputs","2025-12-2.txt")
 # IN_FILE3 = os.path.join("2025","inputs","2025-12-3.txt")
 
 
@@ -18,21 +18,131 @@ def parse(IN_FILE):
     with open(IN_FILE) as fp:
         data = fp.read().splitlines()
 
-    return data
+    barrels = []
+    for r, row in enumerate(data):
+        b_row = []
+        for c, col in enumerate(row):
+            b_row.append(int(col))
+        barrels.append(b_row)
+
+    return barrels
 
 
-def part1(data):           # => 
+def part1(data):           # => 239
+    loc = [[0,0]]
+    destroyed = [[0,0]]
+
+    while loc:
+        r,c = loc.pop()
+        if c+1 < len(data[0]):
+            if data[r][c+1] <= data[r][c]:
+                if [r,c+1] not in destroyed:
+                    destroyed.append([r,c+1])
+                    loc.append([r,c+1])
+        if r+1 < len(data):
+            if data[r+1][c] <= data[r][c]:
+                if [r+1,c] not in destroyed:
+                    destroyed.append([r+1,c])
+                    loc.append([r+1,c])
+        if c-1 >= 0:
+            if data[r][c-1] <= data[r][c]:
+                if [r,c-1] not in destroyed:
+                    destroyed.append([r,c-1])
+                    loc.append([r,c-1])
+        if r-1 >= 0:
+            if data[r-1][c] <= data[r][c]:
+                if [r-1,c] not in destroyed:
+                    destroyed.append([r-1,c])
+                    loc.append([r-1,c])
     
-    return 0
+    return len(destroyed)
 
 
-def part2(data):     # => 
 
-    return 0
+def part2(data):     # => 5594
+    loc = [[0,0]]
+    destroyed = [[0,0]]
+
+    while loc:
+        r,c = loc.pop()
+        if c+1 < len(data[0]):
+            if data[r][c+1] <= data[r][c]:
+                if [r,c+1] not in destroyed:
+                    destroyed.append([r,c+1])
+                    loc.append([r,c+1])
+        if r+1 < len(data):
+            if data[r+1][c] <= data[r][c]:
+                if [r+1,c] not in destroyed:
+                    destroyed.append([r+1,c])
+                    loc.append([r+1,c])
+        if c-1 >= 0:
+            if data[r][c-1] <= data[r][c]:
+                if [r,c-1] not in destroyed:
+                    destroyed.append([r,c-1])
+                    loc.append([r,c-1])
+        if r-1 >= 0:
+            if data[r-1][c] <= data[r][c]:
+                if [r-1,c] not in destroyed:
+                    destroyed.append([r-1,c])
+                    loc.append([r-1,c])
 
 
+    loc = [[len(data)-1, len(data[0])-1]]
+    while loc:
+        r,c = loc.pop()
+        if c+1 < len(data[0]):
+            if data[r][c+1] <= data[r][c]:
+                if [r,c+1] not in destroyed:
+                    destroyed.append([r,c+1])
+                    loc.append([r,c+1])
+        if r+1 < len(data):
+            if data[r+1][c] <= data[r][c]:
+                if [r+1,c] not in destroyed:
+                    destroyed.append([r+1,c])
+                    loc.append([r+1,c])
+        if c-1 >= 0:
+            if data[r][c-1] <= data[r][c]:
+                if [r,c-1] not in destroyed:
+                    destroyed.append([r,c-1])
+                    loc.append([r,c-1])
+        if r-1 >= 0:
+            if data[r-1][c] <= data[r][c]:
+                if [r-1,c] not in destroyed:
+                    destroyed.append([r-1,c])
+                    loc.append([r-1,c])
+
+
+    return len(destroyed)
+
+
+def create_location_grid(data):
+    grid = []
+    for r, row in enumerate(data):
+        for c, _ in enumerate(row):
+            grid.append([r,c])
+    return grid
+
+
+def destroy_barrels(start, barrels) -> list:
+    pass
 
 def part3(data):           # => 
+    max_destroyed = 0
+
+    # create a location map of all the barrels
+    grid = create_location_grid(data)
+
+    for loc in grid[:]:
+        db = destroy_barrels(loc,data)
+        if len(db) > max_destroyed:
+            max_destroyed = len(db)
+            destroyed_barrels = db.copy()
+    
+    
+
+
+
+    
 
     return 0
 
@@ -45,11 +155,11 @@ def solve():
     exec_time = time.time() - start_time
     print(f"part 1: {p1} ({exec_time:.4f} sec)")
 
-    # x = parse(IN_FILE2)
-    # start_time = time.time()
-    # p2 = str(part2(x))
-    # exec_time = time.time() - start_time
-    # print(f"part 2: {p2} ({exec_time:.4f} sec)")
+    x = parse(IN_FILE2)
+    start_time = time.time()
+    p2 = str(part2(x))
+    exec_time = time.time() - start_time
+    print(f"part 2: {p2} ({exec_time:.4f} sec)")
 
     # x = parse(IN_FILE3)
     # start_time = time.time()
