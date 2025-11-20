8f000e0790d8032ebcd517885a55bb9c257db280
Todd Smith <n7tms@yahoo.com>
2025-11-20 16:43:25 -0700

    q14 in progress

--- 2025/q14.py
+++ 2025/q14.py
@@ -19,13 +19,60 @@ def parse(IN_FILE):
     with open(IN_FILE) as fp:
         data = fp.read().splitlines()
 
+    floor = []
+    for r, line in enumerate(data):
+        row = []
+        for c, tile in enumerate(line):
+            if tile == ".":
+                row.append(False)
+            else:
+                row.append(True)
+        floor.append(row)
+    
+    return floor
 
-    return data
+
+def odd_neighbors(f: list, loc: list) -> bool:
+    # return true if this tile needs to change
+
+    DIRs = [[-1,-1],[-1,1],[1,-1],[1,1]]
+
+    a,b = loc
+    neighbors = 0
+    for c,d in DIRs:
+        if a+c >= 0 and b+d >= 0 and a+c < len(f) and b+d < len(f[0]):
+            if f[a+c][b+d]: neighbors += 1
+    
+    if (neighbors in [0,2,4]) and not f[a][b]:
+        return True
+    elif (neighbors in [0,2,4]) and f[a][b] == True:
+        return True
+
+    return False
 
 
-def part1(data):     # => 
+def sum_tiles(f: list) -> int:
+    at = 0
+    for row in f:
+        at += sum(row)
+    
+    return at
 
-    return 0
+
+def part1(data: list):     # => 
+    rounds = 10
+    active_tiles = 0
+    for rnd in range(rounds):
+        tmp_data = data.copy()
+        for r,row in enumerate(data):
+            for c,tile in enumerate(row):
+                if odd_neighbors(data, [r,c]):
+                    tmp_data[r][c] = not tmp_data[r][c]
+
+        active_tiles += sum_tiles(tmp_data)
+        data = tmp_data[:]
+  
+    return active_tiles
 
 
 
